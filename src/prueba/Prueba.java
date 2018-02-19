005f48e4e535bf646f412843e708b9378a7a10f0
Renzon Caceres <rcflechas@gmail.com>
2018-02-19 09:56:33 -0500

    commit rama dos

--- src/prueba/Prueba.java
+++ src/prueba/Prueba.java
@@ -86,40 +86,40 @@ public class Prueba {
      */
     public static void main(String[] args) {  
         //procesarMeses();
-        //ordenarLista();
+        ordenarLista();
         
         
-        int [] lista = {8,9,3};
-        int n = lista.length;
-        String [] simbolo = new String [n];
-        int i,j,aux,izq,der,m;
-        
-        for (i = 1;  i < n; i++) {
-            aux = lista[i]; izq =0; der = i-1;
-            
-            while(izq<=der){
-                m=((izq+der)/2);
-                if(aux < lista[m]){
-                    der = m-1;
-                    simbolo[i-1] = "-";
-                }else{
-                    izq = m+1;
-                    simbolo[i-1] = "+";
-                }
-            }
-            j = i-1;
-            while(j>=izq){
-                lista[j+1] = lista[j];
-                j = j-1;
-            }
-            lista[izq] = aux;
-        }
-        simbolo[i-1] = "$";
-        
-        String salida = "";
-        for(i=0;i<n;i++){
-            salida += lista[i] + simbolo[i];
-        }
-        System.out.println(salida);
+//        int [] lista = {8,9,3};
+//        int n = lista.length;
+//        String [] simbolo = new String [n];
+//        int i,j,aux,izq,der,m;
+//        
+//        for (i = 1;  i < n; i++) {
+//            aux = lista[i]; izq =0; der = i-1;
+//            
+//            while(izq<=der){
+//                m=((izq+der)/2);
+//                if(aux < lista[m]){
+//                    der = m-1;
+//                    simbolo[i-1] = "-";
+//                }else{
+//                    izq = m+1;
+//                    simbolo[i-1] = "+";
+//                }
+//            }
+//            j = i-1;
+//            while(j>=izq){
+//                lista[j+1] = lista[j];
+//                j = j-1;
+//            }
+//            lista[izq] = aux;
+//        }
+//        simbolo[i-1] = "$";
+//        
+//        String salida = "";
+//        for(i=0;i<n;i++){
+//            salida += lista[i] + simbolo[i];
+//        }
+//        System.out.println(salida);
     }    
 }
